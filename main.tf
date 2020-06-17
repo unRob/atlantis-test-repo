4fd5157cbcd2b705427aef5bed75499cf5127485
Roberto Hidalgo <un@rob.mx>
2020-06-16 21:22:40 -0400

    more testing

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
+null_resource useless {
+  triggers = {
+    something = "yup"
+  }
+}
+
+
 output "test" {
   description = "test value"
-  value = "OK"
+  value = "OK: ${null_resource.useless.id}"
 }
